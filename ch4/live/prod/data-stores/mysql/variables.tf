0ea711c372511ca56e288ebbb4d3a1e398e59d6c
SeungAh-Hong <110337062+SeungAh-Hong@users.noreply.github.com>
2023-03-13 14:53:59 +0900

    Delete ch4 directory

--- ch4/live/prod/data-stores/mysql/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "db_password" {
-  type        = string
-  description = "The password for the database"
-}
-
-variable "db_name" {
-    type        = string
-    default     = "database_prod"
-}
